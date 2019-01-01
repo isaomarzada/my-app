49de9b87f663f3470b02d86e8be517c6cf59e9b7
Isa Omarzada <isaomarzada@yahoo.com>
2019-01-01 01:58:53 -0600

    list of available chat rooms rendered to screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RoomList from "./RoomList.js"
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBY71pssu4I5cYjq4xnv1t6d6s0s0m9AKo",
+    authDomain: "my-app-112999.firebaseapp.com",
+    databaseURL: "https://my-app-112999.firebaseio.com",
+    projectId: "my-app-112999",
+    storageBucket: "my-app-112999.appspot.com",
+    messagingSenderId: "902650946653"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RoomList firebase={firebase}/>
       </div>
     );
   }
